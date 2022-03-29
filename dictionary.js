9b57825441a855e4a1a922290f5b00d0272b73aa
Zachary Silva <zsilva@honolulucookie.local>
2022-03-28 15:36:17 -1000

    added click off of dict

--- dictionary.js
+++ dictionary.js
@@ -2,6 +2,7 @@ var popupVisible = false;
 const RIGHTARROW = 39;
 const LEFTARROW = 37;
 const SHIFT = 16;
+var popupDictionaryWindow;
 
 //gets JSON dict as promise
 async function fetchJSONDict() {
@@ -12,7 +13,9 @@ async function fetchJSONDict() {
 //searches for word and returns dictionary entries for word
 async function searchWord(word) {
     definitions = [];
-    word = word.toLowerCase();
+	
+	//trim removes tailing whitespace
+    word = word.toLowerCase().trim();
 
     let response = await fetchJSONDict();
     let data = await response.text();
@@ -38,10 +41,11 @@ function insertPopupDict() {
         var newRange = document.createRange();
         newRange.setStart(selection.focusNode, selection.startOffset);
 
-		var popupDictionaryWindow = document.createElement('span');
+		popupDictionaryWindow = document.createElement('span');
         popupDictionaryWindow.id = 'hawaiian-popup-dictionary'
 		popupDictionaryWindow.style = 'margin-top: 35px; width: 360px;font-size: 14px;font-family: Arial, Helvetica, sans-serif; background-color: #555;color: #fff;border-radius: 6px;padding: 8px 0;position: absolute;z-index: 1;';
-
+		
+		
         //show word and definitions
         searchWord(text).then(definitions => {
 			popupDictionaryWindow.innerHTML = "<p style='text-align: center;'><b style='font-size: 18px;'>" + text + "</b></p>";
@@ -62,6 +66,19 @@ function insertPopupDict() {
 			}
             //place node at newRange instead of range. This is coordinates of popup dict.
 			newRange.insertNode(popupDictionaryWindow);
+			
+			popupVisible = true;
+		
+		
+			// if clicked off window, delete it
+			function clickOffWindow(e){
+				if (popupVisible) {
+					if (!popupDictionaryWindow.innerText.includes(e.target.innerText)){
+						removeDictWindow();
+					}
+				}
+			}
+			document.addEventListener('click', clickOffWindow);
 
 			//if def length > 3 then add pagination functions
             if (definitions.length >= 3) {
@@ -120,7 +137,6 @@ function insertPopupDict() {
                     }
                 }
 		});
-        popupVisible = true;
     }
 }
 
@@ -135,14 +151,18 @@ function getSelectionText() {
 function shiftDowned(e) {
     if(e.keyCode === SHIFT || e.charCode === SHIFT){
         if (popupVisible) {
-            var popupwindow = document.getElementById("hawaiian-popup-dictionary");
-            popupwindow.remove();
-            popupVisible = false;
+			removeDictWindow();
         }
         else {
             insertPopupDict();
         }
     }
+}
+
+function removeDictWindow(){
+	var popupwindow = document.getElementById("hawaiian-popup-dictionary");
+    popupwindow.remove();
+	popupVisible = false;
 }
 
 document.addEventListener('keydown', shiftDowned);
